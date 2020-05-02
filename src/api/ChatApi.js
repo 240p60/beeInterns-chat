6a0a682741d3a1c125a8967312cf622b923ea82b
Rashid <mr.alhoev@mail.ru>
2020-05-02 03:09:23 +0300

    chat ready for evaluation

---
+++ src/api/ChatApi.js
@@ -0,0 +1,34 @@
+export default function ChatApi() {
+    let isStart = false;
+    let numbers = [];
+    return function(cmd) {
+        let reply = `Я не понимаю, введите другую команду!`;
+        if (!isStart && /^\/start$/.test(cmd)) {
+            isStart = true;
+            reply = `Привет, меня зовут Чат-бот, а как зовут тебя?`;
+        } else if (/^\/name: \w+$/.test(cmd)) {
+            let name = cmd.split(' ')[1];
+            reply = `Привет, ${name}, приятно познакомится. Я умею считать, введи числа которые надо посчитать`;
+        } else if (!numbers.length && /^\/number: \d+, \d+/.test(cmd)) {
+            numbers = cmd.split(': ')[1].split(', ').map(item => parseInt(item));
+            reply = `Введите одно из действий: -, +, *, /`;
+        } else if (numbers.length && /^[/+\-*]$/.test(cmd)) {
+            let result;
+            if (cmd === '/')
+                result = numbers[0] / numbers[1];
+            else if (cmd === '*')
+                result = numbers[0] * numbers[1];
+            else if (cmd === '+')
+                result = numbers[0] + numbers[1];
+            else if (cmd === '-')
+                result = numbers[0] - numbers[1];
+            reply = result ? `Результат: ${result}` : `Попробуйте снова`;
+            numbers = [];
+        } else if (/^\/stop$/.test(cmd)) {
+            numbers = [];
+            isStart = false;
+            reply = `Всего доброго, если хочешь поговорить пиши /start`;
+        }
+        return reply;
+    }
+}
---
+++ src/components/ChatMessages/index.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+
+import './ChatMessage.scss';
+import BotAvatar from '../../img/botAvatar.svg';
+import UserAvatar from '../../img/userAvatar.svg';
+
+export default function ChatMessages({ isInputFocused, messageList }) {
+
+  const list = messageList.map(item => {
+    return (
+      <div
+        key={item.id}
+        className="message__item"
+      >
+        <div className="message__item_avatar">
+          <img
+            src={item.isUser? UserAvatar : BotAvatar}
+            alt="avatar icon"
+          />
+        </div>
+        <div
+          className={[
+            "message__item_text",
+            item.isUser ? 'yellow' : 'white'
+            ].join(' ')}
+        >
+          {item.text}
+        </div>
+      </div>
+    )
+  });
+
+  const animationWrite = (
+    <div
+      className="message__item message__animation"
+    >
+      <div className="message__item_avatar">
+        <img
+          src={UserAvatar}
+          alt="avatar icon"
+        />
+      </div>
+      <div className="message__item_text message__animation_write yellow">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+    </div>
+  );
+
+  return (
+    <React.Fragment>
+      {isInputFocused ? animationWrite : null}
+      {list}
+    </React.Fragment>
+  );
+}
